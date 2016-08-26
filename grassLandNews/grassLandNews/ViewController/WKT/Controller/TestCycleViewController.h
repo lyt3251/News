80e597bfb52a03dca219825d62f3b00ac6199364
liu yuantao <lyt0011@163.com>
2016-08-26 20:17:01 +0800

    测试

---
+++ grassLandNews/grassLandNews/ViewController/WKT/Controller/TestCycleViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestCycleViewController.h
+//  grassLandNews
+//
+//  Created by liuyuantao on 16/8/26.
+//  Copyright © 2016年 liuyuantao. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface TestCycleViewController : BaseViewController
+
+@end
---
+++ grassLandNews/grassLandNews/ViewController/WKT/Controller/TestCycleViewController.m
@@ -0,0 +1,37 @@
+//
+//  TestCycleViewController.m
+//  grassLandNews
+//
+//  Created by liuyuantao on 16/8/26.
+//  Copyright © 2016年 liuyuantao. All rights reserved.
+//
+
+#import "TestCycleViewController.h"
+
+@interface TestCycleViewController ()
+
+@end
+
+@implementation TestCycleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
