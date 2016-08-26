6dc17bf497bf12a4e27a0d9fbbc54c75392b630d
liu yuantao <lyt0011@163.com>
2016-08-26 20:21:16 +0800

    测试

--- grassLandNews/grassLandNews/ViewController/WKT/Controller/TestCycleViewController.m
+++ grassLandNews/grassLandNews/ViewController/WKT/Controller/TestCycleViewController.m
@@ -9,7 +9,7 @@
 #import "TestCycleViewController.h"
 
 @interface TestCycleViewController ()
-
+@property(nonatomic, strong)KDCycleBannerView *topView;
 @end
 
 @implementation TestCycleViewController
@@ -18,6 +18,21 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+
+//初始化轮播条
+- (void)initTopCycleView{
+    CGFloat width = kScreenWidth;
+    CGFloat height = kScreenWidth * 420/750;
+    
+    _topView = [[KDCycleBannerView alloc] initWithFrame:CGRectMake(0, 0, width, height)];
+    _topView.delegate = self;
+    _topView.datasource = self;
+    _topView.continuous = YES;
+    _topView.autoPlayTimeInterval = 3.f;
+    [_contentView addSubview:_topView];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -33,5 +48,46 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark - KDCycleBannerViewDataource methods
+- (NSArray *)numberOfKDCycleBannerView:(KDCycleBannerView *)bannerView
+{
+    return _bannerArr;
+}
+- (UIViewContentMode)contentModeForImageIndex:(NSUInteger)index {
+    return UIViewContentModeScaleAspectFill;
+}
+- (UIImage *)placeHolderImageOfZeroBannerView {
+    return [UIImage imageNamed:@"banner.jpg"];
+}
+- (UIImage *)placeHolderImageOfBannerView:(KDCycleBannerView *)bannerView atIndex:(NSUInteger)index
+{
+    return [UIImage imageNamed:@"banner.jpg"];
+}
+- (id)imageSourceForContent:(id)content
+{
+    NSDictionary *dic = (NSDictionary *)content;
+    NSString *urlString = dic[@"DefaultPicUrl"];
+    //对url进行encode编码，避免url中出现中文或者其他非法字符时导致转成NSURL为nil的问题
+    urlString = [urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    //    NSString *imgStr = [urlString getFormatPhotoUrl:_topView.width_ hight:_topView.height_];
+    //    NSString *imgStr = [urlString getFormatPhotoUrl:_topView.width_ hight:_topView.height_];
+    //    return imgStr;
+    return urlString;
+}
+
+-(NSString *)titleAtIndex:(NSUInteger )index;
+{
+    NSDictionary *dic = (NSDictionary *)self.bannerArr[index];
+    NSString *urlString = dic[@"Title"];
+    return urlString;
+}
+
+#pragma mark - KDCycleBannerViewDelegate methods
+- (void)cycleBannerView:(KDCycleBannerView *)bannerView didSelectedAtIndex:(NSUInteger)index {
+    NSDictionary *newsInfo = self.bannerArr[index];
+    NewsDetailViewController *newsDetailVC = [[NewsDetailViewController alloc] initWithNewsId:newsInfo];
+    [self.navigationController pushViewController:newsDetailVC animated:YES];
+}
 
 @end
